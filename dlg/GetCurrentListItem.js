e18180d229175dfc11beec9a6bb9b49629e26873
nicolas <nicolas.matteazzi@gmail.com>
2018-11-24 18:41:13 +0100

    GET /currentList/items/{id}

---
+++ dlg/GetCurrentListItem.js
@@ -0,0 +1,35 @@
+var mongo = require('mongodb');
+var config = require('../config');
+
+var MongoClient = mongo.MongoClient;
+
+exports.do = function(id) {
+
+  return new Promise(function(success, failure) {
+
+    return MongoClient.connect(config.mongoUrl, function(err, db) {
+
+      // Fetch the data!
+      db.db(config.dbName).collection(config.collections.currentList)
+                          .find({_id: new mongo.ObjectId(id)})
+                          .toArray(function(err, array) {
+
+        db.close();
+
+        if (array == null || array.length == 0) return {};
+
+        success({
+          id: item._id,
+          name: item.name,
+          note: item.note,
+          noteBy: item.noteBy,
+          noteByGivenName: item.noteByGivenName,
+          grabbed: item.grabbed,
+          category: item.categoryId
+        })
+
+      });
+    });
+  });
+
+}
